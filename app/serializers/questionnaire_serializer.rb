ce43dc2b6c733ae56832e614fa8aac56d4a53d63
Mauricio <maugravena@gmail.com>
2020-07-14 14:55:30 -0300

    add Model Associations

--- app/serializers/questionnaire_serializer.rb
+++ app/serializers/questionnaire_serializer.rb
@@ -1,12 +1,4 @@
 class QuestionnaireSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :limit_time, :questions
-  def questions
-    self.object.questions.map do |question|
-      {
-        id: question.id,
-        description: question.description,
-        points: question.points
-      }
-    end
-  end
+  has_many :questions
 end
